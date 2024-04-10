2936c6a89df6faac7587ba589e5a97aaca573bbb
Hinga-inc <hingapeter18@gmail.com>
2024-04-10 14:08:56 -0700

    4. Text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,32 @@
+#!usr/bin/python3
+
+def text_indentation(text):
+    """Function that prints text with 2 new lines after each of the \
+characters '.',',', and '?' :
+
+    Args:
+        text (str): text to be edited
+
+    """
+    if type(text) is not str:
+        raise TypeError('text must be a string')
+
+    j = 0
+    delims = '.?:'
+
+    for i, char in enumerate(text):
+        for delim in delims:
+            if char is delim:
+                j += 1
+                text = text[:i + j] + ' ' + text[i + j:]
+
+    list = text.split()
+
+    for word in list:
+        if word[-1:] is "." or word[-1:] is "?" or word[-1:] is ":":
+            print(word, end="\n\n")
+        elif word is list[len(list) - 1]:
+            print(word, end="")
+        else:
+            print(word, end=" ")
+
