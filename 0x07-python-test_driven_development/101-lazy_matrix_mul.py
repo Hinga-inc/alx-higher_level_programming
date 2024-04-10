9d49e9a2e564fcf376be9987fa99ba41bf19558b
Hinga-inc <hingapeter18@gmail.com>
2024-04-10 14:21:33 -0700

    7. Lazy matrix multiplication

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+    a function that multiplies 2 matrices by using the module NumPy
+    prototype def lazy_matrix_mul(m_a, m_b):
+"""
+
+def lazy_matrix_mul(m_a, m_b):
+
+    """
+        returna a matrix that is the product of 2 matrices
+        test case is the same as 100-matrix_muk
+        but with no exeption
+    """
