e4675f3dc1c2b72c58f7ba199c3cdde6587132e2
Hinga-inc <hingapeter18@gmail.com>
2024-04-16 11:38:00 -0700

    0-read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-"""0x0B. Python - Input/Output, task 0. Read file """
-
-
 def read_file(filename=""):
-    """Reads contents of a text file and print to stdout.
-
-    Args:
-        filename (str): name of file to be opened
-
-    """
-    with open(filename, encoding='utf-8') as file:
-        print(file.read(), end='')
+    with open(filename, encoding="utf-8") as fd:
+        for line in fd:
+            print(line, end="")
