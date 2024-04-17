f40372fa98c677673d6789b711b9127e35bded5c
Hinga-inc <hingapeter18@gmail.com>
2024-04-17 15:35:59 -0700

    Write an empty class BaseGeometry

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" 0x0A. Python - Inheritance, task 5 """
+
+
+class BaseGeometry:
+    """Intentionally empty class.
+
+    """
+    pass
+
--- 0x0A-python-inheritance/5-main.py
+++ 0x0A-python-inheritance/5-main.py
@@ -6,3 +6,4 @@ bg = BaseGeometry()
 print(bg)
 print(dir(bg))
 print(dir(BaseGeometry))
+
