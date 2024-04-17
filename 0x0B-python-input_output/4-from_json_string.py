a90443987b5c25b1f9c81a3917f20856cca180da
Hinga-inc <hingapeter18@gmail.com>
2024-04-17 14:50:03 -0700

    Write a function that returns an object (Python data structure) represented by a JSON string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""0x0B. Python - Input/Output, From JSON string to Object  """
+
+
+def from_json_string(my_str):
+    """Returns an object (Python data structure) represented by a JSON string.
+
+    Args:
+        my_obj (any): object to be serialized
+
+    """
+    import json
+
+    return json.loads(my_str)
+
