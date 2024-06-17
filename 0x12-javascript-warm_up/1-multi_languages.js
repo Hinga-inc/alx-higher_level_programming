e401f8f3b9b196810b372ad608a505e194097160
Hinga Peter <hingapeter18@gmail.com>
2024-06-17 20:12:37 +0300

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-const c = 'C is fun';
-const py = 'Python is cool';
-const js = 'JavaScript is amazing';
-
-console.log(c);
-console.log(py);
-console.log(js);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
