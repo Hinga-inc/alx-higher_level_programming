8bb0ad3b97afc3d669518caaff5e32e096758048
Hinga Peter <hingapeter18@gmail.com>
2024-06-19 12:03:53 +0300

    tasks

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+// this is a script that prints 2 arguments passed to it in the format "" is ""
+
+const args = process.argv.slice(2); // the slice(2) excempts the first 2 arguments that are ./filename.js
