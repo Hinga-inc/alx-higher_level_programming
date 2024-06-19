d2655abe24787ff98bf02717bad3daddd769edf0
Hinga Peter <hingapeter18@gmail.com>
2024-06-19 12:13:38 +0300

    value of my argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,8 +2,8 @@
 
 // this is a script that prints the first argument passed to it
 const args = process.argv.slice(2); // the slice(2) excempts the first 2 arguments
-if (args === 0){
-    console.log("No argument");
+if (args === 0) {
+  console.log('No argument');
 } else {
-    console.log(args[0]);
+  console.log(args[0]);
 }
