8d3a8bdf1c55ddb4ddecec466136924adea796d4
Hinga Peter <hingapeter18@gmail.com>
2024-06-19 12:31:07 +0300

    an integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,8 @@
 
 /* this is a script that prints My number: <first argumnet converted into an integer> if it can */
 const args = process.argv.slice(2); /* the slice(2) excempts the first 2 arguments */
-if (args === NaN) {
+const number = parseInt(args[0]);
+if (isNaN(number)) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + parseInt(args[0]));
