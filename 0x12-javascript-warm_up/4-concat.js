16c822668e8a12b04940428580368354be4eb937
Hinga Peter <hingapeter18@gmail.com>
2024-06-19 12:20:41 +0300

    create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,3 +3,4 @@
 // this is a script that prints 2 arguments passed to it in the format "" is ""
 
 const args = process.argv.slice(2); // the slice(2) excempts the first 2 arguments that are ./filename.js
+console.log(args[0] + ' is ' + args[1]);
