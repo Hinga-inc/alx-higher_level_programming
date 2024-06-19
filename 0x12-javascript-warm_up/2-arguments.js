beb85c417a3da1306df24e6875fb3f889bc84eeb
Hinga Peter <hingapeter18@gmail.com>
2024-06-19 12:01:01 +0300

    2 arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
-const args = process.argv.slice(2); // the slice(2) excempts the first 2 arguments
+
+/* this is a script that prints a message depending on the number of args passed*/
+
+const args = process.argv.slice(2); /* the slice(2) excempts the first 2 arguments*/
 
 if (args.length === 0) {
     console.log("No argument");
@@ -8,4 +11,3 @@ if (args.length === 0) {
 } else {
     console.log("Arguments found");
 }
-// this is a script that prints a message depending on the number of args passed
