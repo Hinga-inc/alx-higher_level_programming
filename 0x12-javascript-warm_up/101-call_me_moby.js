9a89c1d523bcd0296ae159c1da088943c4f19d34
Hinga Peter <hingapeter18@gmail.com>
2024-06-19 21:56:21 +0300

    call me mody

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-function callMeMoby(x, theFunction) {
+function callMeMoby (x, theFunction) {
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
-};
+}
 
 module.exports.callMeMoby = callMeMoby;
